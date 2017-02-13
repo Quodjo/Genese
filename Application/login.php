30fc393e611853cba2755451a7e1a00ce5bd71ac
Kojo Nyamekye Anyinam-Boateng <bkojo21@gmail.com>
2017-02-13 08:36:27 +0000

    Update login.php

--- Application/login.php
+++ Application/login.php
@@ -1,20 +1,68 @@
 <!DOCTYPE html>
 <html>
-  <head>
-      <title>Log In || Food</title>
+<head>
+  <title>Log In || Food</title>
 
-       <!--Import Google Icon Font-->
-      <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-      <!--Import materialize.css-->
-      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+  <!--Import Google Icon Font-->
+  <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <!--Import materialize.css-->
+  <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
 
-      <!--Let browser know website is optimized for mobile-->
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-  </head>
-  <body>
+  <link type="text/css" rel="stylesheet" href="css/style.css"/>
+  <!--Let browser know website is optimized for mobile-->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
-      <!--Import jQuery before materialize.js-->
-      <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-      <script type="text/javascript" src="js/materialize.min.js"></script>
-  </body>
+  <link rel="icon" href="Images/logo.png" type="image/png">
+</head>
+<body>
+
+  <img class="responsive-img" src="Images/login_background.jpg"> 
+
+
+  <div class="card bordered z-depth-2 " style="margin-right: 5%; width:35%; float: right;" id="loginCard" >
+
+    <div class="card-header" style=" margin: 0 auto; width: 40%; height: 40%">
+
+      <img class="responsive-img" src="Images/logo.png"> 
+    </div>
+
+    <div class="card-content ">
+      <form>
+        <div class="input-field col s12">
+          <input id="id_number" type="text" class="validate" autofocus>
+          <label for="id_number">ID Number</label>
+        </div>
+        <div class="input-field col s12">
+          <input id="password" type="password" class="validate">
+          <label for="password">Password</label>
+        </div>
+        <div class="input-field col s12 ">
+          <input type="checkbox" class="filled-in" id="filled-in-box"/>
+          <label for="filled-in-box">Remember me</label>
+        </div>
+        <div class="center">
+          <div class="input-field col s12">
+          </br>
+          <button class="waves-effect waves-light btn-large #be1e2d red darken-4" type="submit" name="action" style="width: 100%; font-size: 30px" >Log In</button>
+        </div>
+      </div>
+    </form>
+  </div>
+  <div class="card-action clearfix">
+    <div class="row">
+      <div class="col s6 text-p">
+        <a href="#" style="font-size: 12px" >Forgot Password?</a>
+      </div>
+      <div class="col s6 right-align text-p">
+        <a href="sign_up.php" style="font-size: 12px">Sign Up!</a>
+      </div>
+
+    </div>
+  </div>
+</div>
+
+<!--Import jQuery before materialize.js-->
+<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script type="text/javascript" src="js/materialize.min.js"></script>
+</body>
 </html>
