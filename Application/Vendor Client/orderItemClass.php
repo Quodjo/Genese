ba342bea36fd232ecdeb03a39eb65f4f83998367
Benedict Quartey <hackgh94@gmail.com>
2017-04-08 21:24:35 +0000

    ds

--- Application/Vendor Client/orderItemClass.php
+++ Application/Vendor Client/orderItemClass.php
@@ -1,6 +1,26 @@
 <?php
+/*
+Order item class
+@author: Benedict Quartey
+*/
+
+class orderItem{
+public $orderNum;
+public $userId;
+public $userName;
+public $foodName;
+public $drinkName;
+public $staffOnDuty;
 
 
+function __construct(){
+benedict wrote this
+
+}
+
+
+
+}
 
 
 
