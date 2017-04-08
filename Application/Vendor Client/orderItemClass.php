49afa0a3a22b2eba389d4ace7524f1b77125003a
Benedict Quartey <hackgh94@gmail.com>
2017-04-08 21:46:05 +0000

    add

--- Application/Vendor Client/orderItemClass.php
+++ Application/Vendor Client/orderItemClass.php
@@ -3,20 +3,43 @@
 Order item class
 @author: Benedict Quartey
 */
+require_once('../database/dbconnectionclass.php');
 
 class orderItem{
 public $orderNum;
 public $userId;
+public $foodId;
+public $drinkID;
+public $staffID;
+
+
 public $userName;
 public $foodName;
 public $drinkName;
 public $staffOnDuty;
 
 
-function __construct(){
-benedict wrote this
+function __construct($order,$user,$food,$drink,$staff){
+$this->$orderNum=$order;
+$this->$userId=$user;
+$this->$foodId=$food;
+$this->$drinkId=$drink;
+$this->$staffId=$staff;
+}
+
+function foodDetails(){
+
 
 }
+
+function drinkDetails(){}
+
+function staffDetails(){}
+
+function userDetails(){}
+
+function exportOrder(){}
+
 
 
 
