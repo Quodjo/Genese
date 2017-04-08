664866d0345d3048809ae74939d6f9761a25936e
akosua.adin <akosuadin@gmail.com>
2017-04-08 23:25:46 +0000

    nhg

--- Application/Vendor Client/processapp.php
+++ Application/Vendor Client/processapp.php
@@ -1,20 +1,27 @@
 <?php
 
-require_once('../database/dbconnectionclass.php');
-$sqlquery= "SELECT total_amount FROM menu_order_table where menu_order_id='34'";
+require_once('../Vendor Client/orderItemClass.php');
+// $sqlquery= "SELECT total_amount FROM menu_order_table where menu_order_id='34'";
+//
+//
+// //create a new instance of the database class
+// $verdb = new dbconnection;
+// $dbexec =   $verdb->query($sqlquery);
+//
+// if($dbexec){
+//   $row = $verdb->fetch();
+//   echo ($row['total_amount']);
+// }
+//   else{
+//     echo "Fail";
+//   }
 
 
-//create a new instance of the database class
-$verdb = new dbconnection;
-$dbexec =   $verdb->query($sqlquery);
 
-if($dbexec){
-  $row = $verdb->fetch();
-  echo ($row['total_amount']);
-}
-  else{
-    echo "Fail";
-  }
+
+
+$order = new orderItem("3","4","5","6","7");
+$order->foodDetails();
+
 
 ?>
-    
