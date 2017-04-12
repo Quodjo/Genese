6088f159c51e8b8105970168bcda4007e1a8b3d9
Kojo Nyamekye Anyinam-Boateng <bkojo21@gmail.com>
2017-04-12 17:27:09 +0000

    Add

---
+++ Application/Application/controller/settings/sessionManagement.php
@@ -0,0 +1,42 @@
+<?php
+/**
+*@author Kojo Nyamekye Anyinam-Boateng
+*@version 0.0.0.1
+**/
+
+session_start();
+
+function verifyUserlogin(){
+  //Check For Logiin And Permission
+  if(isset($_SESSION['userID']) && !empty($_SESSION['userID'])){
+    //Valid
+    //Check For User Permission
+    getView();
+  } else{
+
+    //No Valid
+    header('Location: ../login/view/');
+  }
+}
+
+//get user specific header
+function getView(){
+  if($_SESSION['perID']=="1"){
+
+  } else if($_SESSION['perID']=="2"){
+
+  } else if($_SESSION['perID']=="3"){
+
+  } else if($_SESSION['perID']=="4"){
+
+  } else if($_SESSION['perID']=="5"){
+
+  } else if($_SESSION['perID']=="6"){
+
+  }
+
+}
+//Get Current Page
+//Get Previous Page
+//OB Start For Header
+?>
