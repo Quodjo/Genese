22a4434ffed5054d97e12284e1f7725789554420
Kojo Nyamekye Anyinam-Boateng <bkojo21@gmail.com>
2017-04-17 09:18:51 +0000

    S

--- Application/Application/model/LogIn/logInModel.php
+++ Application/Application/model/LogIn/logInModel.php
@@ -2,29 +2,29 @@
 require_once('../database/dbconnection.php');
 
 class LogIn extends dbconnection {
-//function for login
-function verifylogin() {
-  $username = $_POST['username'];
-  $password = $_POST['password'];
+  //function for login
+  function verifylogin() {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 
-  $login = new dbconnection;
+    $login = new dbconnection;
 
-  $sql = "SELECT * FROM useraccount WHERE username = '$username' AND userstatus = 'ACTIVE'";
-  $querySql = $login->query($sql);
-  $data = $login->fetch();
+    $sql = "SELECT * FROM useraccount WHERE username = '$username' AND userstatus = 'ACTIVE'";
+    $querySql = $login->query($sql);
+    $data = $login->fetch();
 
-  if($querySql && password_verify($password, $data["pwd"])){
-    session_start();
-    $_SESSION['userID'] = $data['userid'];
-    $_SESSION['username'] = $data['username'];
-    $_SESSION['perID'] = $data['per_id'];
+    if($querySql && password_verify($password, $data["pwd"])){
+      session_start();
+      $_SESSION['userID'] = $data['userid'];
+      $_SESSION['username'] = $data['username'];
+      $_SESSION['perID'] = $data['per_id'];
 
-    header('Location: ../index.php');
+      header('Location: ../index.php');
+    }
+    else{
+
+    }
+
   }
-  else{
-
-  }
-
-}
 }
 ?>
