e65acfd7b1a176c1bafebdf257fcf849592552ce
Kojo Nyamekye Anyinam-Boateng <bkojo21@gmail.com>
2017-04-19 21:58:35 +0000

    P

--- Application/Application/view/LogIn/index.php
+++ Application/Application/view/LogIn/index.php
@@ -16,8 +16,8 @@
 </head>
 <body>
 	<?php
-		require_once('../../controller/LogIn/logInController.php');
-		?>
+	require_once('../../controller/LogIn/logInController.php');
+	?>
 
 	<div id="container">
 
@@ -89,20 +89,20 @@
 <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
 <script type="text/javascript" src="../../resources/js/materialize.min.js"></script>
 <script>
-	$('.modal').modal({
-      dismissible: true, // Modal can be dismissed by clicking outside of the modal
-      opacity: .5, // Opacity of modal background
-      inDuration: 300, // Transition in duration
-      outDuration: 200, // Transition out duration
-      startingTop: '4%', // Starting top style attribute
-      endingTop: '10%', // Ending top style attribute
-      ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
+$('.modal').modal({
+	dismissible: true, // Modal can be dismissed by clicking outside of the modal
+	opacity: .5, // Opacity of modal background
+	inDuration: 300, // Transition in duration
+	outDuration: 200, // Transition out duration
+	startingTop: '4%', // Starting top style attribute
+	endingTop: '10%', // Ending top style attribute
+	ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
 
-      	console.log(modal, trigger);
-      },
-      complete: function() { } // Callback for Modal close
-  }
-  );
+		console.log(modal, trigger);
+	},
+	complete: function() { } // Callback for Modal close
+}
+);
 
 
 </script>
