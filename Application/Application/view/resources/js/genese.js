c70d7c956573d29bbb6a0327c8ddc5051274b79b
Ronald Nettey <ronaldnettey@yahoo.com>
2017-04-20 00:28:02 +0000

    New version

---
+++ Application/Application/view/resources/js/genese.js
@@ -0,0 +1,71 @@
+$(document).ready(function() {
+
+    startSlide();
+
+
+
+
+    $('#extrasModal').modal();
+
+    $('.cartBtn').click(function() {
+        showCheckout();
+    });
+
+    $('#homeSearch').mousewheel(function(event) {
+        if (event.deltaY == -1) {
+            $('html, body').animate({
+                scrollTop: ($('#foodFeature').offset().top)
+            }, 500);
+        } else if (event.deltaY == 1) {
+
+        }
+
+
+
+    });
+
+    $('#foodFeature').mousewheel(function(event) {
+
+        console.log('ad scrolled' + $('#foodFeature').offset().top);
+        console.log('WIN HEIGHT :' + $('#foodFeature').height());
+
+    });
+
+
+
+
+
+
+
+});
+
+
+
+
+
+
+
+
+/*Slider function*/
+function startSlide() {
+
+    $(".rslides").responsiveSlides({
+        speed: 2000, // Integer: Speed of the transition, in milliseconds
+        timeout: 6000
+    });
+}
+
+/*end of slider function*/
+
+function showCheckout() {
+    var elem = $('.rightSide');
+    elem.toggle(200);
+
+}
+
+function scrollTrigger() {
+
+    $('#homeSearch').mousewheel(function(event) {
+        $('#foodFeature').css('top', '0%');
+    });
+}
