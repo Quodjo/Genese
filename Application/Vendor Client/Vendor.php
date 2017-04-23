8305c7a1ac09123f716c440992f7a7a92111e4a3
Benedict Quartey <hackgh94@gmail.com>
2017-04-23 01:27:35 +0000

    good

--- Application/Vendor Client/Vendor.php
+++ Application/Vendor Client/Vendor.php
@@ -2,9 +2,9 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Vendor</title>
+    <title>Vendor</title>
 
-	<!--Import Google Icon Font-->
+    <!--Import Google Icon Font-->
   <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   <!--Import materialize.css-->
   <link type="text/css" rel="stylesheet" href="../css/materialize.min.css"  media="screen,projection"/>
@@ -34,38 +34,38 @@
 </head>
 <body  style="background-image:url(../images/background_Head.jpg); background-repeat: repeat;overflow-y: scroll;
 border-bottom: 4.5vh;
-	border-style: solid;
-	border-bottom-color: #be1e2d;
+    border-style: solid;
+    border-bottom-color: #be1e2d;
 
 
 ">
          <div class="row">
     <div class="col s2" style=" margin-top: 0.5%">
-    	
+        
  <span >
-    	<ul id="slide-out" class="side-nav">
-		      <li><div class="userView">
-		        <div class="background">
-		          <div class="card-panel" style="background: rgba(100, 0, 0, 0.7);height:100%"></div>
-		        </div>
-		        <a href="#!user"><img class="circle" src="../images/admin.jpg"></a>
-		        <a href="#!name"><span class="white-text name">John Doe</span></a>
-		        <a href="#!email"><span class="white-text email">john doe@gmail.com</span></a>
-		      </div></li>
-		      <br><br><br><br><br><br><br><br><br>
-		      <br>
+        <ul id="slide-out" class="side-nav">
+              <li><div class="userView">
+                <div class="background">
+                  <div class="card-panel" style="background: rgba(100, 0, 0, 0.7);height:100%"></div>
+                </div>
+                <a href="#!user"><img class="circle" src="../images/admin.jpg"></a>
+                <a href="#!name"><span class="white-text name">John Doe</span></a>
+                <a href="#!email"><span class="white-text email">john doe@gmail.com</span></a>
+              </div></li>
+              <br><br><br><br><br><br><br><br><br>
+              <br>
 
-		      <li><a href="Vendor.php"><i class="material-icons" style="font-size: 4rem; padding-left: 35%">toll</i><br><h6 align =center>Orders</h3></a></li><br>
-		      <li><div class="divider"></div></li>
-		      <li><a href="Inventory.php"><i class="material-icons" style="font-size: 4rem;padding-left: 35%">view_module</i> <br><h6 align =center>Inventory</h3></a></li>
+              <li><a href="Vendor.php"><i class="material-icons" style="font-size: 4rem; padding-left: 35%">toll</i><br><h6 align =center>Orders</h3></a></li><br>
+              <li><div class="divider"></div></li>
+              <li><a href="Inventory.php"><i class="material-icons" style="font-size: 4rem;padding-left: 35%">view_module</i> <br><h6 align =center>Inventory</h3></a></li>
 
-		    </ul>
-		    <a href="#" data-activates="slide-out" class="button-collapse"><i class="material-icons" style="font-size: 3.7rem;color:white;">menu</i></a>
+            </ul>
+            <a href="#" data-activates="slide-out" class="button-collapse"><i class="material-icons" style="font-size: 3.7rem;color:white;">menu</i></a>
 </span>
-		
-	<span style="padding-left:9%">
-		<img  class="responsive-img" src="../images/whiteLogo.png" style="width:55%;height: 55%">
-	</span>
+        
+    <span style="padding-left:9%">
+        <img  class="responsive-img" src="../images/whiteLogo.png" style="width:55%;height: 55%">
+    </span>
 
     </div>
 
@@ -77,49 +77,51 @@ border-bottom: 4.5vh;
 
     <div align= center class="col s8" style="font-family: ‘Courier New’, Courier, monospace; padding-top: 1.6% ">
 
-    	<h4 style="color: white">ORDERS</h4>
+        <h4 style="color: white">ORDERS</h4>
 
     </div>
     <div class="col s2" >
-    	<div align=right style="color: white">
-    	<img class="circle responsive-img" src="../images/admin.jpg" style="width:35%;height: 35%;border: 0.25rem solid #be1e2d;">KOJO DOE
-    	</div>
+        <div align=right style="color: white">
+        <img class="circle responsive-img" src="../images/admin.jpg" style="width:35%;height: 35%;border: 0.25rem solid #be1e2d;">KOJO DOE
+        </div>
     </div>
 </div>
 
 <div class="row" >
     <div class="col s12 m6 l8" style="padding-top:8%">
 
- 		<div  class="row" align="center" >
- 		<div id="orderContainer">
- 		
- 			</div>
- 		</div>
+        <div  class="row" align="center" >
+        <div id="orderContainer">
+        
+            </div>
+        </div>
      
 
     </div>
-	<!-- 
+    <!-- 
 in kitchen and processed order column -->
     <div class="col s12 m6 l4" >
 <div style="padding-right: 35px">
-	    <div align="center" class="row" style=" padding-top: 15%;font-family: ‘Courier New’, Courier, monospace;" >
+        <div align="center" class="row" style=" padding-top: 15%;font-family: ‘Courier New’, Courier, monospace;" >
 
-	    	<div class="card grey darken-1" style="height:40px"><span style="color:white" class="card-title">IN KITCHEN</span>
+            <div class="card grey darken-1" style="height:40px"><span style="color:white" class="card-title">IN KITCHEN</span>
 
-				</div>
+                </div>
 
-			<div class="card blue-grey darken-1" > <!-- optimal card height:550px-->
-				            <div class="card-content white-text">
-				              <span class="card-title"></span>
-				              <p>Processed orders go here<br><br>
-				              </p>
-	
-				            </div>
+            <div class="card blue-grey darken-1" > <!-- optimal card height:550px-->
+                            <div class="card-content white-text">
+                              
+                              <p>Processed orders go here<br><br>
+                              </p>
+                              <div id="kitchen">
+                              
+    
+                            </div>
 
-			 </div>
+             </div>
 
 
-	    </div>
+        </div>
 
     </div>
 
@@ -138,33 +140,33 @@ in kitchen and processed order column -->
 
 <div id="modalcontainer"></div>
 <!--modal to create the pop ups-->
-				<div align=center id="modal1" class="modal" style="width:480px">
-					<div class="modal-content">
-					<div style="border:solid grey 3px">
-						<h4 id="ordernum" style="font-size:350%;"><  #BB-ORDER NO.</h4><br>
+                <div align=center id="modal1" class="modal" style="width:480px">
+                    <div class="modal-content">
+                    <div style="border:solid grey 3px">
+                        <h4 id="ordernum" style="font-size:350%;"><  #BB-ORDER NO.</h4><br>
          <div class="divider red darken-4" style="height:5px"></div>
-						<p1 id="username"style ="font-size:170%;color:red">20202018:John Doe </p1><br>
-						</div>
+                        <p1 id="username"style ="font-size:170%;color:red">20202018:John Doe </p1><br>
+                        </div>
 
 
-		  <div><p1 id="username"style ="font-size:170%">Food</p1></div>
+          <div><p1 id="username"style ="font-size:170%">Food</p1></div>
             <div class="grey">
             <p2 style ="font-size:180%" id="foodid">Jollof + Fish  1/2P</p2><br><br>
           </div>
-		  <p1 id="username"style ="font-size:170%">Drinks</p1>
-		  <div class="divider red darken-4" style="height:5px"></div>
+          <p1 id="username"style ="font-size:170%">Drinks</p1>
+          <div class="divider red darken-4" style="height:5px"></div>
               <div class="divider grey lighten-5" style="height:8px" ></div>
             <div class="grey" >
             <pre id="drinkid" style ="font-size:150%">Khebab Sausage style  2
             Vita Milk  2
             Bottled Water  2</pre>
             </div>
-					</div>
-					<div id="btnholder" class="modal-footer">
-						
+                    </div>
+                    <div id="btnholder" class="modal-footer">
+                        
 
-					</div>
-				</div>
+                    </div>
+                </div>
 
   <div align=center class="row" style="padding-top: 5%">
  
@@ -174,10 +176,11 @@ in kitchen and processed order column -->
   
 
 <button onclick="createButtons()" id="demo">Click me</button> //test button
+<button onclick="loadkitchen()" >kitchen</button> //test button
 
 
 <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-	<script type="text/javascript" src="../js/materialize.min.js"></script>
+    <script type="text/javascript" src="../js/materialize.min.js"></script>
 
 
 <script>
@@ -193,11 +196,12 @@ they arent reloaded*/
 
 //generate a unique modal containing details of button that calls it. Called in create button method
 function modalGen(index){
-	jsonOrder = JSON.parse(myObj[index]);
+    jsonOrder = JSON.parse(myObj[index]);
 
 
 var username=document.getElementById('username');
-					username.innerHTML="Username : " +jsonOrder.userFname+" "+jsonOrder.userLname;
+    var fullname = jsonOrder.userFname+" "+jsonOrder.userLname;
+                    username.innerHTML="Username : " +fullname;
 
 var orderid=document.getElementById('ordernum');
 orderid.innerHTML="#BB-ORDER NO. "+jsonOrder.orderNum;
@@ -214,6 +218,22 @@ kitchenBtn.className="modal-action modal-close orange waves-effect waves- btn-fl
 kitchenBtn.innerHTML="Add to kitchen";
 kitchenBtn.id=index;
 
+
+kitchenBtn.onclick= function(){
+    $.post("processapp.php", //Required URL of the page on server
+    { // Data Sending With Request To Server
+    name:fullname,
+    orderNum:jsonOrder.orderNum,
+    foodname:jsonOrder.foodname,
+    drinkname:jsonOrder.drinkname,
+    foodid:jsonOrder.foodid,
+    action: 'kitchen'
+    },
+    function(response,status){ // Required Callback Function
+    alert(response);
+    });
+}
+
 btnHolder.appendChild(kitchenBtn);
 
  $('#modal1').modal('open');
@@ -222,7 +242,7 @@ btnHolder.appendChild(kitchenBtn);
 }
 
 // function moveKitchen(){
-// 	alert("sucess");
+//  alert("sucess");
 // $dbcon = new dbconnection;
 // $sql="INSERT INTO in_kitchen(orderNum,username,foodname,drinkname) VALUES() ";
 //  $sql="INSERT INTO webtechtable(username,gender,color) VALUES (\"$username\",\"$gender\",\"$color\")" ; 
@@ -270,30 +290,31 @@ $(".button-collapse").sideNav();
       outDuration: 200, // Transition out duration
       startingTop: '4%', // Starting top style attribute
       endingTop: '10%', // Ending top style attribute
-      	ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
-      	//alert("MOVES TO KITCHEN");
-      	//console.log(modal, trigger);
+        ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
+        //alert("MOVES TO KITCHEN");
+        //console.log(modal, trigger);
       },
       complete: function() { 
+          
+/*
+           var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
 
-		   var xmlhttp = new XMLHttpRequest();
-	xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+                alert(this.responstText);
 
-		if (this.readyState == 4 && this.status == 200) {
-				alert(this.responstText);
+        } 
+        xmlhttp.open("POST", "processapp.php?action=addToKitchen", true);
 
-		}
-		xmlhttp.open("POST", "processapp.php?action=addToKitchen", true);
-
-	xmlhttp.send();
+    xmlhttp.send();
     } //end of create button method
 
-
+*/
  
-	$( '#btnholder' ).empty();
-	
-	} // Callback for Modal close
-	//var btnHolder = document.getElementById("btnholder" );
+    $( '#btnholder' ).empty();
+    
+    } // Callback for Modal close
+    //var btnHolder = document.getElementById("btnholder" );
   }
   );
 
@@ -301,60 +322,104 @@ $(".button-collapse").sideNav();
 
 
   function createButtons() {
-	 // $( '#orderContainer' ).empty(); //clear space to avoid duplication of buttons
+     // $( '#orderContainer' ).empty(); //clear space to avoid duplication of buttons
 
-	
-   var xmlhttp = new XMLHttpRequest();
-	xmlhttp.onreadystatechange = function() {
-		if (this.readyState == 4 && this.status == 200) {
-			myObj = JSON.parse(this.responseText);
+    $.post("processapp.php", //Required URL of the page on server
+{ // Data Sending With Request To Server
+action: 'createBtn'
+},
+function(response,status){ // Required Callback Function
+myObj = JSON.parse(response);
 
-			//foreach chunk that converts each item in the response array into json object
-			//and creates buttons that hold their details
-			myObj.forEach(function(currentValue, index, arr){
-				if(myObj[index])
-				jsonOrder = JSON.parse(myObj[index]);
+            //foreach chunk that converts each item in the response array into json object
+            //and creates buttons that hold their details
+            myObj.forEach(function(currentValue, index, arr){
+                if(myObj[index])
+                jsonOrder = JSON.parse(myObj[index]);
 
 
 
-				var container=document.getElementById('orderContainer');
-				var btn = document.createElement("button");
-				btn.className="btn waves-effect waves-light";
-				btn.id=jsonOrder.orderNum;
-					btn.style.width = "160px";
-					btn.style.height = "120px";
-					btn.style.background = "rgba(110,0,0,0.7)";
-					btn.style.color = "white";
-					ordercount =index+1;
-					btn.innerHTML = "Order 0"+ordercount;
-					
-				btn.onclick= function(){
-					modalGen(index);
-					$(this).removeClass("waves-effect waves-light submit").addClass('disabled');
+                var container=document.getElementById('orderContainer');
+                var btn = document.createElement("button");
+                btn.className="btn waves-effect waves-light";
+                btn.id=jsonOrder.orderNum;
+                    btn.style.width = "160px";
+                    btn.style.height = "120px";
+                    btn.style.background = "rgba(110,0,0,0.7)";
+                    btn.style.color = "white";
+                    ordercount =index+1; 
+                    btn.innerHTML = "Order 0"+ordercount;
+                    
+                btn.onclick= function(){
+                    modalGen(index);
+                    $(this).removeClass("waves-effect waves-light submit").addClass('disabled');
 
 }; //end of button onclick anonymous function
 
-				container.appendChild(btn);
+                container.appendChild(btn);
 
 
 
-			}); //end of for each item in json array/ or in order database
-	
-		} //end of if readystate==4 check
-	}; //end of ajax on readystatecheck anonymous funciton 
+            }); //end of for each item in json array/ or in order database
+//alert("*----Received Data----*\n\nResponse : " + response+"\n\nStatus : " + status);//"response" receives - whatever written in echo of above PHP script.
+
+});
 
 
-	xmlhttp.open("POST", 
-	"processapp.php", true);
+  
 
-	xmlhttp.send();
+
+    
     } //end of create button method
 
 
-function loadButton() {
+function loadkitchen() {
+
+    var container=document.getElementById('kitchen');
 
 
+    var caseHorizontal = document.createElement("div");
+                caseHorizontal.className="card horizontal";
 
+                var case0 = document.createElement("div");
+                case0.className="class-image";
+
+                var img = document.createElement("img");
+                img.src="../images/jollof.jpg";
+                img.style.width="60%"
+                img.style.height="60%"
+
+                case0.appendChild(img);
+                caseHorizontal.appendChild(case0);
+
+                var case1 = document.createElement("div");
+                case1.className="card-stacked";
+
+                var case2 = document.createElement("div");
+                case2.className="card-content";
+
+                var name =document.createElement("h5");
+                name.innerHTML="Jollof ";
+
+                case2.appendChild(name);
+                case1.appendChild(case2);
+
+                var case3 = document.createElement("div");
+                case3.className="card-action waves-effect waves-green";
+                case3.style.background="rgba(0,110,0,0.7)";
+
+                var link = document.createElement("a");
+                link.style.color="white";
+                link.innerHTML="Deliver";
+                
+
+                case3.appendChild(link);
+                case1.appendChild(case3);
+
+                caseHorizontal.appendChild(case1);
+                
+                
+                container.appendChild(caseHorizontal);
 }
 
 
@@ -365,3 +430,5 @@ function loadButton() {
 </body>
 </html>
 
+
+          
--- Application/Vendor Client/processapp.php
+++ Application/Vendor Client/processapp.php
@@ -10,24 +10,43 @@ require_once('../database/dbconnectionclass.php');
 //Array to hold collective order objects created from data in database
 $orderArray=array();
 
-export();
-//    initialize();
-//  foreach($GLOBALS['orderArray'] as $order){
-//     echo $order;
-//     echo"<br>";
-//     }
+/* These conditions allows multiple ajax calls to be made from front end*/
+if(isset($_POST['action']) && !empty($_POST['action'])) {
+    $action = $_POST['action'];
+    if($action=='kitchen'){
+        addToKitchen();
+    }
 
-if(isset($_POST['action']) && function_exists($_POST['action'])) {
-  echo (addToKitchen());
-}
+    if($action=='createBtn'){
+        export();
+    }  
+
+    }
+
+
 
 function addToKitchen(){
+
+if($_POST["name"])
+{
+$name = $_POST["name"];
+$orderNum=$_POST["orderNum"];
+$foodname=$_POST["foodname"];
+$drinkname=$_POST["drinkname"];
+$foodid=$_POST["foodid"];
+
+echo $foodid;
+/*
+//get foodicon from food table using foodid
+
+//putting into in_kitchen table of database
     $dbcon = new dbconnection;
-    $sql="INSERT INTO in_kitchen(orderNum,username,foodname,drinkname) VALUES (\"$username\",\"$gender\",\"$color\")" ;  //sql query for insertion
-	$dbexec = $dbcon->query($sql);
-	if($dbexec){return "success";}
-	else{return "failed";}
-	
+    $sql="INSERT INTO in_kitchen(orderNum,username,foodname,drinkname) VALUES (\"$orderNum\",\"$name\",\"$foodname\",\"$drinkname\")" ;  //sql query for insertion
+    $dbexec = $dbcon->query($sql);
+    if($dbexec){return "success";}
+    else{return "failed";}
+    */
+}
 }
 
 
@@ -84,6 +103,7 @@ echo $JSONArray;
 
 
 ?>
+
 
 
 
