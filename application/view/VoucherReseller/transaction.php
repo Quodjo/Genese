d6629eaf8b31e739419ad028957ecb93fc638c47
Kojo Nyamekye Anyinam-Boateng <bkojo21@gmail.com>
2017-04-26 18:19:22 +0000

    Ok

--- application/view/VoucherReseller/transaction.php
+++ application/view/VoucherReseller/transaction.php
@@ -29,42 +29,54 @@
       <h2><strong>TRANSACTIONS</strong></h2>
     </div>
 
-    <div class="fixed-action-btn horizontal">
-     <a href="hompage.php" class="btn-floating btn-large" id = "hbtn">
-      <i class="large material-icons" style="font-size: 3rem;">add</i>
-    </a>
+    <!-- <div class="row" style="margin-top:20%">
+    <div class="col s10 offset-s1">
+    <div class="card blue-grey darken-1">
+    <div class="card-content white-text">
+    <span class="card-title">Card Title</span>
+    <p>I am a very simple card. I am good at containing small bits of information.
+    I am convenient because I require little markup to use effectively.</p>
   </div>
+</div>
+</div>
+</div> -->
 
-  <ul id="slide-out" class="side-nav">
-    <li><a href="transaction.php"><i class="material-icons">store</i></a></li>
-    <li><a href="hompage.php"><i class="material-icons">credit_card</i></a></li>
-    <li><a href="../LogIn/logout.php" id="logOut" class="center"><i class="material-icons">lock</i></a></li>
-  </ul>
-  <a href="#slide-out" data-activates="slide-out" class="button-collapse show-on-large" ><i class="material-icons" style="font-size: 3rem;" id ="menuBtn">menu</i></a>
+<div class="fixed-action-btn horizontal">
+  <a href="hompage.php" class="btn-floating btn-large" id = "hbtn">
+    <i class="large material-icons" style="font-size: 3rem;">add</i>
+  </a>
+</div>
 
-  <div class="row valign-wrapper" id="nameDisplay" style="top:6%;">
-<?php userName(); ?>
-  </div>
+<ul id="slide-out" class="side-nav">
+  <li><a href="transaction.php"><i class="material-icons">store</i></a></li>
+  <li><a href="hompage.php"><i class="material-icons">credit_card</i></a></li>
+  <li><a href="../LogIn/logout.php" id="logOut" class="center"><i class="material-icons">lock</i></a></li>
+</ul>
+<a href="#slide-out" data-activates="slide-out" class="button-collapse show-on-large" ><i class="material-icons" style="font-size: 3rem;" id ="menuBtn">menu</i></a>
+
+<div class="row valign-wrapper" id="nameDisplay" style="top:6%;">
+  <?php userName(); ?>
+</div>
 
 
 
-  <!--Import jQuery before materialize.js-->
-  <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-  <script type="text/javascript" src="../../resources/js/materialize.min.js"></script>
-  <script>
+<!--Import jQuery before materialize.js-->
+<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script type="text/javascript" src="../../resources/js/materialize.min.js"></script>
+<script>
 // Initialize collapse button
 $(".button-collapse").sideNav();
-  // Initialize collapsible (uncomment the line below if you use the dropdown variation)
-  // $('.collapsible').collapsible();
-  $('.button-collapse').sideNav({
-      menuWidth: 90, // Default is 300
-      edge: 'right', // Choose the horizontal origin
-      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
-      draggable: true // Choose whether you can drag to open on touch screens
-    }
-    );
+// Initialize collapsible (uncomment the line below if you use the dropdown variation)
+// $('.collapsible').collapsible();
+$('.button-collapse').sideNav({
+  menuWidth: 90, // Default is 300
+  edge: 'right', // Choose the horizontal origin
+  closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+  draggable: true // Choose whether you can drag to open on touch screens
+}
+);
 
-  </script>
+</script>
 
 </body>
 </html>
